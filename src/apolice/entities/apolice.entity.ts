d4c4a1b12287fe909dccd2a205cc68b01f56fb0c
vinii-pimentel <vinii_2302@hotmail.com>
2026-02-02 22:50:02 -0300

    Fix doc do Swagger

--- src/apolice/entities/apolice.entity.ts
+++ src/apolice/entities/apolice.entity.ts
@@ -2,38 +2,46 @@ import { IsNotEmpty } from "class-validator";
 import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Categoria } from "../../categoria/entities/categoria.entity";
 import { Usuario } from "../../usuario/entities/usuario.entity";
+import { ApiProperty } from "@nestjs/swagger"
 
 @Entity({name: "tb_apolices"})
 export class Apolice {
 
     @PrimaryGeneratedColumn()
+    @ApiProperty()
     id: number 
 
     @IsNotEmpty()
     @Column({length: 100, nullable: false})
+    @ApiProperty()
     titulo: string
 
     @IsNotEmpty()
     @Column({nullable: false})
+    @ApiProperty()
     valor: number    
 
     @IsNotEmpty()
     @Column({nullable: false})
+    @ApiProperty()
     status: boolean    
 
     @IsNotEmpty()
     @Column({length: 1000, nullable: false})
+    @ApiProperty()
     descricao: string
 
 
     @ManyToOne(() => Categoria, (categoria) => categoria.apolice, {
         onDelete: "CASCADE"
     })
+    @ApiProperty()
     categoria: Categoria
 
     @ManyToOne(() => Usuario, (usuario) => usuario.apolice, {
         onDelete: "CASCADE"
     })
+    @ApiProperty()
     usuario: Usuario
     
 }
--- src/categoria/entities/categoria.entity.ts
+++ src/categoria/entities/categoria.entity.ts
@@ -1,24 +1,29 @@
 import { IsNotEmpty } from "class-validator";
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Apolice } from "../../apolice/entities/apolice.entity";
+import { ApiProperty } from "@nestjs/swagger"
 
 
 @Entity({ name: "tb_categorias" })
 export class Categoria {
 
     @PrimaryGeneratedColumn()
+    @ApiProperty()
     id: number
 
     @IsNotEmpty()
     @Column({ length: 255, nullable: false })
+    @ApiProperty()
     nome: string
 
     @IsNotEmpty()
     @Column({ length: 255, nullable: false })
+    @ApiProperty()
     descricao: string
 
     // Indica o lado UM do relacionamento, indicando que esse campo se conecta ao campo Usuario da Model Produto
     @OneToMany(() => Apolice, (apolice) => apolice.categoria)
+    @ApiProperty()
     apolice: Apolice[]
 
 }
